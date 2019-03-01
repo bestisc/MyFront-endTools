fa50733dede5446af9ee9abc41c8e9f8cee6580f
liuxinghong <liuxinghong@300.cn>
2019-03-01 17:59:55 +0800

    定义cookie对象的API

--- cookie.js
+++ cookie.js
@@ -58,3 +58,53 @@ function getCookie(){
 
 //	注意cookie 的 path 属性, domain 属性, secure 属性
 
+
+
+//实现基于cookie的存储API
+//
+/**
+ * [CookieStorage 本类模仿localStorage和sessionStorage一样的存储API(构造函数)，
+ * 不同的是基于HTTP cookie实现它]
+ *
+ * @param  {[type]} maxage [存储有效期]
+ * @param  {[type]} path   [作用域]
+ */
+function CookieStorage(maxage,path){
+
+	//获取一个存储全部cookie信息的对象
+	//初始化一个cookie对象
+	var cookie = (function(){
+		//初始化要返回的对象
+		var cookieObject = {};	
+
+		//获取cookie的所有信息				
+		var allCookie = document.cookie;
+
+		//如果为空字符串，返回一个空对象	
+		if(allCookie === ""){
+			return cookieObject;
+		}
+
+		//把cookie分离成键值对的字符串数据
+		var cookieArray = allCookie.split("; ");
+
+		for(var i = 0; i < cookieArray.length; i++){
+			//遍历每个cookieArray
+			var cookie = cookieArray[i];
+			var index = cookie.indexOf("=");		//查找第一个“=”服务
+			var key = cookie.substring(0,index);		//获取cookie的key值
+			var value = cookie.substring(index + 1);	//获取cookie的value值
+			value = decodeURIComponent(value);		//对value值进行解码
+			cookieObject[key] = value;				//将键值对存储到对象中
+		}	
+		//console.log(cookieObject);
+		return cookieObject;
+	})();
+
+	//将所有cookie的键名存储到一个数组中
+	var cookieNameArray = [];
+	for(var key in cookie){
+		cookieNameArray.push(key);
+	}
+}
+
